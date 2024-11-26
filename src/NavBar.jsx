1f6dbecacef9335630af7459ccd47332c96359d5
Vaishnavi Nimrot <152794017+vaishnavinimrot@users.noreply.github.com>
2024-11-26 12:33:57 +0530

    Added icons to navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import 'font-awesome/css/font-awesome.min.css'; // Import Font Awesome
 
- // Optional: Add styles specific to Navbar
+// Optional: Add styles specific to Navbar
 
 function Navbar() {
   return (
@@ -9,20 +10,25 @@ function Navbar() {
       <div className="navbar-container">
         <h1 className="logo">Hospital Management System</h1>
         <ul className="nav-links">
+         
           <li>
-            <Link to="/admin/Adminlogin">Admin Login</Link>
+            <Link to="/user/UserLogin">
+              <i className="fas fa-user"></i> User
+            </Link>
           </li>
           <li>
-            <Link to="/user/UserLogin">User Login</Link>
+            <Link to="/user/Register">
+              <i className="fas fa-user-plus"></i> Register
+            </Link>
           </li>
           <li>
-            <Link to="/user/Register">User Register</Link>
+            <Link to="/admin/Adminlogin">
+              <i className="fas fa-user-shield"></i> Admin
+            </Link>
           </li>
         </ul>
       </div>
-      
     </nav>
-    
   );
 }
 
